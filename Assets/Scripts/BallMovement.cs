e6ca59d30d93ca377ee291c35595f0e2242ca69b
lucywalt <lucywltrs@gmail.com>
2025-01-28 14:28:57 -0800

    Added ball script
    Created ball movement script and added to Sphere as a component

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Calling the start method");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Calling the update method");
+        
+    }
+}
