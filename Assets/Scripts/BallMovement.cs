777739a8541ed511d9744d0af5e69deaa9bc8d45
lucywalt <lucywltrs@gmail.com>
2025-01-28 14:41:04 -0800

    Key controls in Ball movement
    Added key controls for wasd, and Vector2 object in update() for ballmovement

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,13 +5,37 @@ public class BallMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Calling the start method");
+        //Debug.Log("Calling the start method");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Calling the update method");
-        
+        // Debug.Log("Calling the update method");
+        Vector2 inputVector = Vector2.zero;
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+
+        Debug.Log("Resultant Vector: " + inputVector);
+
     }
 }
